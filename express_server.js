ef8fed5c9fc2c3c713313d9470be3c7d1d631288
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-23 19:59:44 +0000

    adding new endpoints to show that a local varibale in a route cannot be used in another route

--- express_server.js
+++ express_server.js
@@ -9,14 +9,24 @@ const urlDatabase = {
 
 app.get('/', (req, res) => {
   res.send('Hello!');
-})
+});
 app.get('/urls.json', (req, res) => {
   res.send(urlDatabase);
-})
+});
 
 app.get('/hello', (req, res) =>{
   res.send('<html><body> Hello <b>World</b></body></html>\n')
+});
+
+app.get('/set', (req, res) =>{
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get('/fetch', (req, res) => {
+  res.send(`a = ${a}`);
 })
+
 app.listen(PORT, () =>{
   console.log(`Example app listening on port ${PORT}!`)
 });
