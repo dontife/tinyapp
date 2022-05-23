f57d8837985edc26791ad2e6703adc561cf8ff57
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-23 23:57:31 +0000

    installed the body-parser middleware, getting ready for POST requests and I implemented a function that returns a string of 6 random alphanumeric characters

--- express_server.js
+++ express_server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app =  express();
 const PORT = 8080; // default port 8080
 
-app.set('view engine', 'ejs');
 
+function generateRandomString() {
+  Array.from(Array(6), () => 
+  Math.floor(Math.random() * 36).toString(36)).join('');
+}
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -17,23 +24,32 @@ app.get('/urls', (req, res) =>{
   res.render('urls_index', templateVars);
 
 });
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL : urlDatabase[req.params.shortURL]}
-  res.render('urls_show', templateVars); 
-})
 app.get('/urls.json', (req, res) => {
   res.send(urlDatabase);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/hello', (req, res) =>{
   res.send('<html><body> Hello <b>World</b></body></html>\n')
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+})
+
 app.get('/set', (req, res) =>{
   const a = 1;
   res.send(`a = ${a}`);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL : urlDatabase[req.params.shortURL]}
+  res.render('urls_show', templateVars); 
+})
 app.get('/fetch', (req, res) => {
   res.send(`a = ${a}`);
 })
